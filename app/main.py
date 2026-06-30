1e2f1f69d02f4db486059e0333b8231156ddd31d
anacarmona10 <anacarmona1023@gmail.com>
2026-06-30 00:22:05 -0500

    Ya está CORS

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes.user_routes import router
 
 from app.database.connection import engine, Base
@@ -16,6 +17,17 @@ app = FastAPI(
     title="device_systems API",
     description="API REST segura para gestión de usuarios, dispositivos y préstamos",
     version="3.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://localhost:3000"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
 )
 
 app.include_router(auth_router)
